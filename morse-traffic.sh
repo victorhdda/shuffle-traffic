9951fbf660878b3b1a310d92911bf1ebf8cd3885
Victor Davila <victorhdd@gmail.com>
2020-05-23 22:27:31 -0300

    added slash caracter

--- morse-traffic.sh
+++ morse-traffic.sh
@@ -2,14 +2,16 @@
 #Move to validation and then pull request to master
 #Morse traffic generator
 #Usage message:
-[ $# -eq 0 ] && { echo -e "Usage: $0 [IP address] [time(seconds)] '[morse_code]' \n IP address of IPERF3 server \n Time is the trasnmission's duration of every code \n morse_code should be put between '' \nSOS Ex: $0 127.0.0.1 5 '...---...'"; exit 1; }
+[ $# -eq 0 ] && { echo -e "Usage: $0 [IP address] [time(seconds)] '[morse_code]' \n IP address of IPERF3 server \n Time is the trasnmission's duration of every code \n morse_code should be put between ''. Accepter caracters dot(.) dash (-) back slash (/) space ( )  \nS O S Ex: $0 127.0.0.1 5 '... / --- / ...'"; exit 1; }
 
 #Set vars
 server=$1
 timex=$2
 morse=$3
 
-echo The following message $morse will be sent to iperf3 server at IP: $server in intervals of $timex [s].
+echo $num_char
+
+echo -e "The following message $morse will be sent to iperf3 server at IP: $server in intervals of $timex [s]. \nThis will take $(($timex * ${#morse})) [s]"
 
 #Set double duration for dash caracter
 timex2=$((timex * 2))
@@ -31,10 +33,16 @@ for (( i=0; i<${#morse}; i++ )); do
     iperf3 -c $server -R -Z -t $timex2 -b $max -p 5202 >/dev/null #download
     wait
 
-  elif [[ "$var" == " " ]]; then
+  elif [[ "$var" == " "  ]]; then
     sleep $timex # sleep interval for space caracter
     wait
-  fi
   sleep $timex # sleep interval between caracters
+
+  elif [[ "$var" == "/"  ]]; then
+    #echo "/"
+    sleep $timex # sleep interval for space caracter
+  wait
+fi
+sleep $timex # sleep interval between caracters
 done
 exit
